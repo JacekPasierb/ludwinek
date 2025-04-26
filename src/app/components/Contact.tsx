7358f8b625447ffb4427412c80cedfcd688c7ea2
Jacek Pasierb <jack_byk@o2.pl>
2025-04-26 16:09:32 +0200

    Fix/Contact - poprawiono iframe

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -18,7 +18,7 @@ export default function Contact() {
             <iframe
               className={styles.map}
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2461.793739907407!2d22.840986!3d51.331465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4722be1e67a2cfa3%3A0x64eb5f6df54f6e3c!2sLudwinek!5e0!3m2!1spl!2spl!4v1617123123456!5m2!1spl!2spl"
-              allowFullScreen=""
+              allowFullScreen={true}
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
               title="Mapa dojazdu"
