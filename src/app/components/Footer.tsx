7f92bf12ba8c3c455aa1a53bb43b82eccb5521f7
Jacek Pasierb <jack_byk@o2.pl>
2025-04-26 17:17:49 +0200

    Fix/Footer - rozbudowano footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,25 +1,71 @@
-"use client" 
+"use client";
 
 import React from "react";
 import styles from "../styles/footer.module.css";
-import { usePathname } from "next/navigation";
+import {FaPhone, FaMapMarkerAlt, FaFacebookF} from "react-icons/fa";
+import Image from "next/image";
 
-const Footer = () => {
-  const pathname = usePathname();
-  const isAdmin = pathname.startsWith("/admin");
-
-  if(isAdmin) return null
+export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerInner}>
-        <div>© 2025 Łowisko Eko-Torf Ludwinek</div>
-        <div className={styles.footerLinks}>
-          <a href="/polityka-prywatnosci">Polityka prywatności</a>
-          <a href="mailto:kontakt@ekotorf.pl">kontakt@ekotorf.pl</a>
+      <div className={styles.container}>
+        <div className={styles.column}>
+          <div className={styles.logo}>
+            <Image
+              src="/logoW.png"
+              alt="Ludwinek logo"
+              className={styles.logoImg}
+              width={230}
+              height={128}
+            />
+          </div>
+          <div className={styles.icons}>
+            <a href="tel:600123456">
+              <FaPhone />
+            </a>
+            <a
+              href="https://maps.google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaMapMarkerAlt />
+            </a>
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF />
+            </a>
+          </div>
         </div>
+
+        <div className={styles.column}>
+          <h4>Dane kontaktowe:</h4>
+          <p>📍 Ludwin, Lubelskie</p>
+          <p>📞 600 123 456</p>
+          <p>✉ kontakt@ekotorf.pl</p>
+        </div>
+
+        <div className={styles.column}>
+          <h4>Łowisko:</h4>
+          <a href="/regulamin">Regulamin</a>
+          <a href="/mapa-lowiska">Mapa Łowiska</a>
+          <a href="/rezerwacje">Rezerwacje</a>
+        </div>
+
+        <div className={styles.column}>
+          <h4>Menu:</h4>
+          <a href="/o-lowisku">O łowisku</a>
+          <a href="/galeria">Galeria</a>
+          <a href="/kontakt">Kontakt</a>
+        </div>
+      </div>
+
+      <div className={styles.bottomBar}>
+        © {new Date().getFullYear()} Łowisko Eko-Torf Ludwinek. Wszelkie prawa
+        zastrzeżone.
       </div>
     </footer>
   );
-};
-
-export default Footer;
+}
